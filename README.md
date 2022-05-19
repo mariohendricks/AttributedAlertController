729080b8a65cd9d1f22d3d632bff11faf0f51340
Mario Hendricks <mariohendricks@yahoo.com>
2022-05-19 18:15:06 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AttributedAlertController
+
+A description of this package.
---
+++ Tests/AttributedAlertControllerTests/AttributedAlertControllerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AttributedAlertController
+
+final class AttributedAlertControllerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AttributedAlertController().text, "Hello, World!")
+    }
+}
